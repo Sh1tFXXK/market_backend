a52cccf82fab68a3ae8112636431b480779e69cf
Sh1tFXXK <139066523+Sh1tFXXK@users.noreply.github.com>
2025-05-06 01:00:49 +0800

    添加TokenBO业务对象

---
+++ src/main/java/org/example/market_backend/BO/TokenBO.java
@@ -0,0 +1,11 @@
+package org.example.market_backend.BO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class TokenBO {
+    private String userId;
+    private String mobile;
+}
